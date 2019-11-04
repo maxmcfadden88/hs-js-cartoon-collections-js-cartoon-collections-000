3548ed3628472c7a0aebbfb1f50a2e7ea6361386
Max McFadden <maxmcfadden88@gmail.com>
2019-11-04 17:34:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 function dwarfRollCall(dwarves) {
+let dwarfString = '';
+for (var i = 0; i < dwarves.length; i++) {
+dwarfString += (i+1).toString()+ ". " + dwarves[i] + ' ';
+}
+return dwarfString;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(fruits){
+var newFruits = [];
+for (var i = 0; i < fruits.length; i++) {
+newFruits.push(fruits[i].toUpperCase() + "!");
+}
+return newFruits;
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(element) {
+for(let i = 0; i < element.length; i++){
+if(element[i].length>4){
+return true}
 }
+return false;
+}
+
 
 function findTheCheese (foods) {
+for (var i = 0; i < foods.length; i++) {
+if (foods[i] === 'cheddar')
+return foods[i];
+}
+return 'no cheese!';
 }
